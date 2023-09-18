4211bee510e3ae8ede8de900fafd3e015cb760b5
Kinanee Samson <kinaneesamsonjohn@gmail.com>
2023-09-18 14:26:11 +0100

    tested create account and login

--- src/data-source.ts
+++ src/data-source.ts
@@ -5,8 +5,8 @@ require('dotenv').config();
 
 export const AppDataSource = new DataSource({
     type: "mysql",
-    host: "db4free.net",
-    port: 3306,
+    host: process.env.DB_HOST,
+    port: parseFloat(process.env.DB_PORT),
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB,
